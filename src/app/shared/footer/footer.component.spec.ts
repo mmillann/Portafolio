aa615f030dd9d4293c8aad381d39464a44c6e9df
Manuel <mme0008@alu.medac.es>
2022-05-23 14:25:48 +0200

    Template listo

---
+++ src/app/shared/footer/footer.component.spec.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector:'app_header',
+  templateUrl: './footer.component.html',
+  styleUrls:['./footer.component.css']
+})
+export class FooterComponent implements OnInit{
+  constructor(){}
+  ngOnInit() {
+    
+  }
+}
---
+++ src/app/shared/header/header.component.spec.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector:'app_header',
+  templateUrl: './header.component.html',
+  styleUrls:['./header.component.css']
+})
+export class HeaderComponent implements OnInit{
+  constructor(){}
+  ngOnInit() {
+    
+  }
+}
+
